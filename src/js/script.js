a988916399f515f277acf0f63d4f3a6390bf1dd5
Mathis-Juillard <mathis.juillard@edu.univ-fcomte.fr>
2024-12-04 14:34:55 +0100

    ouvrir par un click et fermer par un click

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,20 @@
 // *** LIGHTBOX ***
 const lightbox = document.querySelector("#lightbox");
 
-lightbox.showModal ();
-
 lightbox.addEventListener("click", function() {
   lightbox.close();
 }); 
 
-const lightboxImg = document.querySelectorAll("img");   
+const images = document.querySelectorAll("img");   
 
 images.forEach(image=> {
     image.addEventListener("click", function() {
        const fullImg = image.dataset.fullImg;
-       const imgElement = document.querySelector("#lightbox img");
-        lightboxImg.src = image.src;
+       const imgElement = lightbox.querySelector("img");
+        imgElement.src = fullImg;
         lightbox.showModal();
     });
-    }
+    });
 
 
 
