51b5ebf0443b1f5ad24c5856b9b7abef79295c3e
Mathis-Juillard <mathis.juillard@edu.univ-fcomte.fr>
2024-12-04 14:30:28 +0100

    suite du js

--- src/js/script.js
+++ src/js/script.js
@@ -5,5 +5,20 @@ lightbox.showModal ();
 
 lightbox.addEventListener("click", function() {
   lightbox.close();
-});
+}); 
+
+const lightboxImg = document.querySelectorAll("img");   
+
+images.forEach(image=> {
+    image.addEventListener("click", function() {
+       const fullImg = image.dataset.fullImg;
+       const imgElement = document.querySelector("#lightbox img");
+        lightboxImg.src = image.src;
+        lightbox.showModal();
+    });
+    }
+
+
+
+
 
